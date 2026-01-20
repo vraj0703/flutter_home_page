38a029c800f4659dd5bd8909f1ef3031a89e7a7f
Vishal Raj <vraj0703@gmail.com>
2026-01-20 22:18:33 +0530

    chore: bold text

--- lib/project/app/system/scroll_controller/bold_text_controller.dart
+++ lib/project/app/system/scroll_controller/bold_text_controller.dart
@@ -1,17 +1,12 @@
 import 'package:flame/components.dart';
-import 'package:flutter_home_page/project/app/curves/exponential_ease_out.dart';
 import 'package:flutter_home_page/project/app/interfaces/scroll_observer.dart';
 import 'package:flutter_home_page/project/app/views/components/bold_text/bold_text_reveal_component.dart';
 import 'package:flutter_home_page/project/app/config/scroll_sequence_config.dart';
-import 'package:flutter_home_page/project/app/views/my_game.dart';
 
 class BoldTextController implements ScrollObserver {
   final BoldTextRevealComponent component;
   final double screenWidth;
   final Vector2 centerPosition;
-
-  bool _hasPlayedEntrySound = false;
-  bool _hasPlayedExitSound = false;
 
   BoldTextController({
     required this.component,
